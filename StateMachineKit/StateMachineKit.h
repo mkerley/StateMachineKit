05437bedb89aa95518b949e6a248398e35f55898
Michael Kerley <mkerley@gmail.com>
2017-10-01 23:54:54 -0700

    Added iOS target.
    Removed macOS-specific #import Cocoa.h from StateMachineKit.h

--- StateMachineKit/StateMachineKit.h
+++ StateMachineKit/StateMachineKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Michael Kerley. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for StateMachineKit.
 FOUNDATION_EXPORT double StateMachineKitVersionNumber;
